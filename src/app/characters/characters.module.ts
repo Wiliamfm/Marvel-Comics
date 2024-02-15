894496925f0eb07f0d6e87f47009bf4dd4912bd2
william <william.fonseca@pevaar.com>
2024-02-15 12:46:17 -0500

    create characters component

---
+++ src/app/characters/characters.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CharactersRoutingModule } from './characters-routing.module';
+import { CharactersComponent } from './characters/characters.component';
+
+
+@NgModule({
+  declarations: [
+    CharactersComponent
+  ],
+  imports: [
+    CommonModule,
+    CharactersRoutingModule
+  ],
+  exports: [
+    CharactersComponent
+  ]
+})
+export class CharactersModule { }
