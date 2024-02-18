3f24e9c9d9a015be0cf62e5abfeddd4d5a574b15
William <santifon15@gmail.com>
2024-02-18 13:02:30 -0500

    add basic routing & services

--- src/app/characters/characters.module.ts
+++ src/app/characters/characters.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CharactersRoutingModule } from './characters-routing.module';
-import { CharactersComponent } from './characters/characters.component';
 import { CharacterDetailsComponent } from './character-details/character-details.component';
 import { CharacterCardComponent } from './character-card/character-card.component';
 import { FavoritesComponent } from './favorites/favorites.component';
+import { CharactersComponent } from './characters.component';
 
 
 @NgModule({
