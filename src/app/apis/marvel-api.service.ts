4838f57ee8a82f7e34795908d0a71be7280bc4ba
William <santifon15@gmail.com>
2024-02-19 08:38:51 -0500

    add marvel api

--- src/app/apis/marvel-api.service.ts
+++ src/app/apis/marvel-api.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MarvelApiService {
+  private readonly baseUrl = "https://gateway.marvel.com:443/v1/public";
+  private readonly apiKey = "";
 
-  constructor() { }
+  constructor(private readonly _httpClient: HttpClient) { }
+
+  private createUrl(url: string): string{
+    return `${this.baseUrl}${url}?apikey=${this.apiKey}`;
+  }
+
+  public getCharacters() {
+    return this._httpClient.get<any[]>(this.createUrl('/characters'));
+  }
 }
