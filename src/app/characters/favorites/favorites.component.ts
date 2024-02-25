54ba7dd79e31c4b5ff452b7f4b73bb164ac89480
William <santifon15@gmail.com>
2024-02-25 09:04:00 -0500

    Add favorite page

--- src/app/characters/favorites/favorites.component.ts
+++ src/app/characters/favorites/favorites.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ComicService } from 'src/app/comics/comic.service';
+import { Comic } from 'src/app/models/comics';
 
 @Component({
   selector: 'app-favorites',
   templateUrl: './favorites.component.html',
   styleUrl: './favorites.component.scss'
 })
-export class FavoritesComponent {
+export class FavoritesComponent implements OnInit{
+  favorites: Comic[]= [];
+
+  constructor(private readonly _comicService: ComicService){
+
+  }
+
+  ngOnInit(): void {
+    this.favorites = this._comicService.favoriteComics;
+  }
+
+  createImgUrl(item: Comic, imgSize?: string) {
+    return this._comicService.createImgUrl(item, imgSize);
+  }
+
+  removeComic(comic: Comic) {
+    this._comicService.removeFavorite(comic);
+    //this.favorites = this._comicService.favoriteComics;
+  }
 
 }
--- src/app/comics/comic.service.ts
+++ src/app/comics/comic.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { MarvelApiService } from '../apis/marvel-api.service';
 import { Comic, GetComic } from '../models/comics';
 import { Observable } from 'rxjs';
+import { PORTRAIT_FANTASTIC_168X252PX } from '../characters/const/character';
 
 @Injectable({
   providedIn: 'root'
@@ -25,6 +26,10 @@ export class ComicService {
     });
   }
 
+  createImgUrl(item: Comic, imgSize = PORTRAIT_FANTASTIC_168X252PX) {
+    return `${item.thumbnail.path}/${imgSize}.${item.thumbnail.extension}`
+  }
+
   checkComicInFavorites(comic: Comic): boolean {
     for(const favoriteComic of this.favoriteComics) {
       if(favoriteComic.id === comic.id) {
@@ -32,6 +37,24 @@ export class ComicService {
       }
     }
     return false;
+  }
+
+  getComicFromFavorites(comicId: number): Comic | null {
+    for(const favoriteComic of this.favoriteComics) {
+      if(favoriteComic.id === comicId) {
+        return favoriteComic;
+      }
+    }
+    return null;
+  }
+
+  getComicIndexFromFavorites(comicId: number): number {
+    for(let i = 0; i < this.favoriteComics.length; i++) {
+      if(this.favoriteComics[i].id === comicId) {
+        return i;
+      }
+    }
+    return -1;
   }
 
   addFavorite(comic: Comic): boolean {
@@ -44,8 +67,8 @@ export class ComicService {
   }
 
   removeFavorite(comic: Comic): boolean {
-    const comicIndex = this.favoriteComics.indexOf(comic);
-    if(comicIndex < 0) {
+    const comicIndex = this.getComicIndexFromFavorites(comic.id);
+    if(comicIndex === -1) {
       return false;
     }
     this.favoriteComics.splice(comicIndex, 1);
